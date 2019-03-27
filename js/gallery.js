e1cf9ed0e4c4a4dc2136c375f148b750e4b3b76d
royer-z <43219292+royer-z@users.noreply.github.com>
2019-03-27 14:08:01 -0400

    Task: created GalleryImage objects for each image in the JSON object and stored them into an array

--- js/gallery.js
+++ js/gallery.js
@@ -32,6 +32,18 @@ function animate() {
 
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
+//implement me as an object to hold the following data about an image:
+//1. location where photo was taken
+//2. description of photo
+//3. the date when the photo was taken
+//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+function GalleryImage(location, description, date, img) {
+    this.location = location;
+    this.description = description;
+    this.date = date;
+    this.img = img;
+}
+
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
 	//Access the img element and replace its source
@@ -65,6 +77,11 @@ var mJson = JSON.parse(mRequest.response);
 // Array holding GalleryImage objects (see below).
 var mImages = [];
 
+mJson.images.forEach(image => {
+    mImages.push(new GalleryImage(image.imgLocation, image.Description, image.date, image.imgPath));
+});
+console.log(mImages);
+
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
 function makeGalleryImageOnloadCallback(galleryImage) {
@@ -86,16 +103,3 @@ window.addEventListener('load', function() {
 	console.log('window loaded');
 
 }, false);
-
-function GalleryImage() {
-	this.location;
-	this.description;
-	this.date;
-	this.img;
-
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-}
